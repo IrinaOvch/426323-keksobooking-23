96513916eb13231436542cb29f38b7f5d256387f
Irina Ovchinnikova <iovchinnikova@outlook.com>
2021-07-02 17:19:26 +0200

    Fix bugs

--- js/map.js
+++ js/map.js
@@ -1,11 +1,18 @@
 import { setActiveState } from './form.js';
 import {createPropertyOffers} from './mock-data.js';
-import {fillPropertyOffer} from './render-property-offers.js';
+import {fillPropertyOffer} from './render-offer.js';
 
-const TOKYO_COORDS = {
-  lat: 35.6821594,
-  lng: 139.7447689,
+const adressInput = document.querySelector('#address');
+
+const TileLayer = {
+  URL: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  ATTRIBUTION: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 };
+const TokyoCoords = {
+  LAT: 35.6821594,
+  LNG: 139.7447689,
+};
+const DIGITS = 5;
 const ZOOM_LEVEL = 13;
 const PIN_SIZE = 40;
 const MAIN_PIN_SIZE = 52;
@@ -14,27 +21,26 @@ const OFFERS_AMOUNT = 10;
 const mainPinIcon = L.icon({
   iconUrl: '../img/main-pin.svg',
   iconSize: [MAIN_PIN_SIZE, MAIN_PIN_SIZE],
-  iconAnchor: [MAIN_PIN_SIZE/2, MAIN_PIN_SIZE],
+  iconAnchor: [MAIN_PIN_SIZE / 2, MAIN_PIN_SIZE],
 });
 
 const pinIcon = L.icon({
   iconUrl: '../img/pin.svg',
   iconSize: [PIN_SIZE, PIN_SIZE],
-  iconAnchor: [PIN_SIZE/2, PIN_SIZE],
+  iconAnchor: [PIN_SIZE / 2, PIN_SIZE],
 });
 
 const mainPin = L.marker(
-  TOKYO_COORDS,
+  [TokyoCoords.LAT, TokyoCoords.LNG],
   {
     draggable: true,
     icon: mainPinIcon,
   },
 );
 
-
 const renderPins = (array, map) => {
-  array.forEach(({author, offer, location}) => {
-    const {lat, lng} = location;
+  array.forEach((offer) => {
+    const {lat, lng} = offer.location;
     const marker = L.marker({
       lat,
       lng,
@@ -46,7 +52,7 @@ const renderPins = (array, map) => {
     marker
       .addTo(map)
       .bindPopup(
-        fillPropertyOffer({author, offer, location}),
+        fillPropertyOffer(offer),
         {
           keepInView: true,
         },
@@ -56,25 +62,23 @@ const renderPins = (array, map) => {
 
 const mainPinMoveHandler = (evt) => {
   const {lat, lng} = evt.target.getLatLng();
-  const adForm = document.querySelector('.ad-form');
-  const adressInput = adForm.querySelector('#address');
-  adressInput.value = `${lat}, ${lng}`;
+  adressInput.value = `${lat.toFixed(DIGITS)}, ${lng.toFixed(DIGITS)}`;
 };
 
 const setMap = () => {
   const map = L.map('map-canvas')
     .on('load', setActiveState)
-    .setView(TOKYO_COORDS, ZOOM_LEVEL);
+    .setView([TokyoCoords.LAT, TokyoCoords.LNG] , ZOOM_LEVEL);
 
   L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    TileLayer.URL,
     {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      attribution: TileLayer.ATTRIBUTION,
     },
   ).addTo(map);
   mainPin.addTo(map);
 
-  mainPin.on('moveend', mainPinMoveHandler);
+  mainPin.on('move', mainPinMoveHandler);
   renderPins(createPropertyOffers(OFFERS_AMOUNT), map);
 };
 
